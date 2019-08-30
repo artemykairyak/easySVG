7e13364fa1e82b275d4585b985d07bd848c9e230
Artemy Kairyak <artemykairyak@yandex.com>
2019-08-30 21:25:57 +0400

    version 2

--- src/easySVG.js
+++ src/easySVG.js
@@ -1,17 +1,28 @@
-$('.svg').each(function() {
-	var path = '../' + $(this).data('src');
-	replace($(this), path);
+const svgArr = document.querySelectorAll('.svg');
+
+svgArr.forEach(function(item) {
+	const path = item.dataset.src;
+	replaceSvg(item, path);
 });
 
-function replace(elem, path) {
-	var s = new XMLSerializer();
-	
-	$.ajax({
-    	url: path,
-        success: function(data) {
-			var str = s.serializeToString(data);
-        	elem.append(str);
-        	elem.find('svg').unwrap();
-      	}
+function replaceSvg(elem, path) {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', path);
+	request.addEventListener("readystatechange", () => {
+		if (request.readyState === 4 && request.status === 200) {
+	      const attr = elem.getAttribute('class');
+	      const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
+	      
+	      elem.insertAdjacentHTML('afterend', request.responseText);
+
+	      if(noSvgClassAttr.length) {
+	      	 elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
+	      }
+	     
+	      elem.remove();
+	    }
 	});
+
+	request.send();
 }
