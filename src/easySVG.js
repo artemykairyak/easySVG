07fe566082265467d7c6b42c800168261903c6a6
artemykairyak <artemykairyak@yandex.com>
2019-09-10 11:10:35 +0400

    added IE support

--- src/easySVG.js
+++ src/easySVG.js
@@ -1,28 +1,48 @@
 const svgArr = document.querySelectorAll('.svg');
 
-svgArr.forEach(function(item) {
-	const path = item.dataset.src;
-	replaceSvg(item, path);
-});
+for(var i = 0; i < svgArr.length; i++) {
+	const path = svgArr[i].dataset.src;
+	replaceSvg(svgArr[i], path);
+}
 
 function replaceSvg(elem, path) {
-	const request = new XMLHttpRequest();
+	if('ActiveXObject' in window){
+		let IErequest = new ActiveXObject('MSXML2.XMLHTTP');
+		IErequest.onReadyStateChange = function() {
+			if (IErequest.readyState === 4 && IErequest.status === 200) {
+				const attr = elem.getAttribute('class');
+				const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
 
-	request.open('GET', path);
-	request.addEventListener("readystatechange", () => {
-		if (request.readyState === 4 && request.status === 200) {
-	      const attr = elem.getAttribute('class');
-	      const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
-	      
-	      elem.insertAdjacentHTML('afterend', request.responseText);
+				elem.insertAdjacentHTML('afterend', IErequest.responseText);
+				
+				if(noSvgClassAttr.length) {
+					elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
+				}
 
-	      if(noSvgClassAttr.length) {
-	      	 elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
-	      }
-	     
-	      elem.remove();
-	    }
-	});
+				elem.parentNode.removeChild(document.querySelector('div.svg'));
+			}
+		}
+		IErequest.open('GET', path);
+		IErequest.send();
 
-	request.send();
+	} else{
+		let request = new XMLHttpRequest();
+		request.open('GET', path);
+		request.addEventListener("readystatechange", () => {
+			if (request.readyState === 4 && request.status === 200) {
+				const attr = elem.getAttribute('class');
+				const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
+
+				elem.insertAdjacentHTML('afterend', request.responseText);
+
+				if(noSvgClassAttr.length) {
+					elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
+				}
+
+				elem.remove();
+			}
+		});
+
+		request.send();
+	}
 }
