1e4a0f16c445bf9809f3c66ad049e2574d16cf9d
artemykairyak <artemykairyak@yandex.com>
2020-02-20 15:01:44 +0400

    removing redundant commentNodes and textNodes

--- src/easySVG.js
+++ src/easySVG.js
@@ -15,6 +15,27 @@ function easySVG() {
     }
 }
 
+function removeBadNodes() {
+    var treeWalker = document.createTreeWalker(document, NodeFilter.SHOW_COMMENT, null, false);
+    var nodeList = [];
+    while (treeWalker.nextNode()) {
+        if (treeWalker.currentNode.data.indexOf('?xml') !== -1) {
+            nodeList.push(treeWalker.currentNode);
+            if (treeWalker.currentNode.nextSibling.nodeType === 3) {
+                nodeList.push(treeWalker.currentNode.nextSibling);
+            }
+        } else if (treeWalker.currentNode.data.indexOf('Adobe') !== -1) {
+            nodeList.push(treeWalker.currentNode);
+            if (treeWalker.currentNode.nextSibling.nodeType === 3) {
+                nodeList.push(treeWalker.currentNode.nextSibling);
+            }
+        }
+    }
+    for (var i = 0; i < nodeList.length; i++) {
+        nodeList[i].parentNode.removeChild(nodeList[i])
+    }
+};
+
 function replaceSvg(elem, path) {
     if ('ActiveXObject' in window) {
         let IErequest = new ActiveXObject('MSXML2.XMLHTTP');
@@ -23,13 +44,12 @@ function replaceSvg(elem, path) {
                 const attr = elem.getAttribute('class');
                 const noSvgClassAttr = attr.slice(attr.indexOf('svg') + 3);
 
-                if (elem.parentNode) {
+                if (path && elem.parentNode) {
                     elem.insertAdjacentHTML('afterend', IErequest.responseText);
-
                     if (noSvgClassAttr.length) {
+                        removeBadNodes();
                         elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
                     }
-
                     elem.parentNode.removeChild(document.querySelector('.svg'));
                 }
             }
@@ -47,16 +67,14 @@ function replaceSvg(elem, path) {
 
                 if (path && elem.parentNode) {
                     elem.insertAdjacentHTML('afterend', request.responseText);
-
                     if (noSvgClassAttr.length) {
+                        removeBadNodes();
                         elem.nextSibling.setAttribute('class', noSvgClassAttr.trim());
                     }
                 }
-
                 elem.remove();
             }
         });
-
         request.send();
     }
 }
